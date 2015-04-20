16fdd956f72c4036cba697f7f5b61ddb0e1762cb
James Dixon <jim.w.dixon@gmail.com>
2015-04-19 23:09:15 -0600

    updated `initializers/auth.js` to be `initializers/firebase-auth.js`, merged PR

--- app/authenticators/firebase.js
+++ app/authenticators/firebase.js
@@ -4,62 +4,69 @@ import config from '../config/environment';
 
 export default Base.extend({
 
-	init: function() {
-		if(config.firebase) {
-			this.set('firebase', new Firebase(config.firebase));
-		} else {
-			throw new Error(new Error("'firebase' not defined in environment"));
-		}
+    init: function() {
+        if (config.firebase) {
+            this.set('firebase', new Firebase(config.firebase));
+        } else {
+            throw new Error("'firebase' not defined in environment");
+        }
 
-		this._super();
-	},
-	firebase: null,
+        this._super();
+    },
+    firebase: null,
     restore: function(data) {
 
-    	return new Promise(function(resolve, reject) {
+        var _this = this;
+
+        return new Promise(function(resolve, reject) {
 
             if (data.token) {
 
-        		this.get('firebase').authWithCustomToken(data.token, function(error, success) {
-        			Ember.run(function() {
-	            		if(error) {
-	            			reject(error);
-	            		} else {
-	            			resolve(success);
-	            		}
-	            	});
-            	});
+                _this.get('firebase').authWithCustomToken(data.token, function(error, success) {
+                    Ember.run(function() {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve(success);
+                        }
+                    });
+                });
 
             } else {
-            	reject(new Error('Unable to restore Firebase session: no token found.'));
+                reject(new Error('Unable to restore Firebase session: no token found.'));
             }
-        	
-        }.bind(this));
+
+        });
 
     },
     authenticate: function(options) {
 
-        return new Promise(function(resolve, reject) {
-        	
-	        this.get('firebase').authWithPassword({
-	            'email': options.email,
-	            'password': options.password
-	        }, function(error, authData) {
-	            Ember.run(function() {
-	                if (error) {
-	                    reject(error);
-	                } else {
-	                    resolve(authData);
-	                }
-	            });
+        var _this = this;
 
-	        });
-        }.bind(this));
+        return new Promise(function(resolve, reject) {
+
+            _this.get('firebase').authWithPassword({
+                'email': options.email,
+                'password': options.password
+            }, function(error, authData) {
+                Ember.run(function() {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(authData);
+                    }
+                });
+
+            });
+        });
     },
     invalidate: function(data) {
-   		return new Promise(function(resolve, reject) {
-            this.get('firebase').unauth();
-    		resolve(data);
-	    }.bind(this));
+
+        var _this = this;
+
+        return new Promise(function(resolve, reject) {
+            _this.get('firebase').unauth();
+            resolve(data);
+        });
     }
 });

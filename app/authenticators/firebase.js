030dcf1accba5b762c7998f500912f1b7c6ee7d8
bttf <adnanchowdhury88@gmail.com>
2016-04-16 00:39:03 -0700

    make compatible with ember-simple-auth v1.1.0-beta4

--- app/authenticators/firebase.js
+++ app/authenticators/firebase.js
@@ -1,4 +1,4 @@
-import Base from 'simple-auth/authenticators/base';
+import Base from 'ember-simple-auth/authenticators/base';
 import Firebase from 'firebase';
 import config from '../config/environment';
 
